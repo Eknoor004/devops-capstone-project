e258bfc6cc0733df7fd6b9d7c8fcfe3a84e8fd04
Eknoor004 <Eknoormeetsingh@gmail.com>
2026-05-28 19:59:02 -0400

    Add update account endpoint with tests

--- service/routes.py
+++ service/routes.py
@@ -87,7 +87,17 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """Updates an Account"""
+    app.logger.info("Request to update an Account for id: [%s]", account_id)
+    check_content_type("application/json")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND)
+    account.deserialize(request.get_json())
+    account.update()
+    return jsonify(account.serialize()), status.HTTP_200_OK
 
 
 ######################################################################
@@ -112,3 +122,4 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
+
