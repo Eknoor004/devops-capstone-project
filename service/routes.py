6bc8feb0b2389495f3b85a2fb7adcbf801c6f812
Eknoor004 <Eknoormeetsingh@gmail.com>
2026-05-28 20:08:24 -0400

    Add delete account endpoint with tests

--- service/routes.py
+++ service/routes.py
@@ -92,6 +92,15 @@ def update_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """Deletes an Account"""
+    app.logger.info("Request to delete an Account for id: [%s]", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
+
 ######################################################################
 # U T I L I T Y   F U N C T I O N S
 ######################################################################
